d45ea7db981aef73963fd7d82210d1615a1a9ff2
salman-farcy <salmanfarcy253@gmail.com>
2024-02-05 11:07:48 +0600

    simple folder base routing system adds  Next.js sum add code

---
+++ src/app/products/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Products = () => {
+     return (
+          <div>
+               <h1>This is a Products page</h1>
+          </div>
+     );
+};
+
+export default Products;
