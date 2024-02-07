a8f1c5ca36789cdbf8374576d920eef4c0ccc8a0
salman-farcy <salmanfarcy253@gmail.com>
2024-02-07 13:17:20 +0600

    not-found page create in next.js

---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+
+
+const NotFoundPage = () => {
+     return (
+          <div className="flex items-center justify-center h-screen ">
+               <h1 className="font-bold text-4xl ">404 ??? Page Not found</h1>
+          </div>
+     );
+};
+
+export default NotFoundPage;
