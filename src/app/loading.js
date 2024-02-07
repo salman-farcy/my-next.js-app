1e70e2955255974962b34370f9f9dc073203c4e6
salman-farcy <salmanfarcy253@gmail.com>
2024-02-07 16:29:38 +0600

    create loading page in next.js

---
+++ src/app/loading.js
@@ -0,0 +1,11 @@
+
+
+const LoadingPage = () => {
+     return (
+          <div>
+               <h1 className='text-3xl'>Loadng...</h1>
+          </div>
+     );
+};
+
+export default LoadingPage;
