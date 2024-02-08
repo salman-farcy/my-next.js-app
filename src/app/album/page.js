987a4f3ff8b0c0c0c7ef0ad753871a7a9c176f7f
salman-farcy <salmanfarcy253@gmail.com>
2024-02-08 22:21:11 +0600

    Add local image next.js

--- src/app/album/page.js
+++ src/app/album/page.js
@@ -1,16 +1,26 @@
 import Image from "next/image";
-
+import nextImage from "@/assets/nextjs3.webp"
 
 const AlbumPage = () => {
      return (
           <div>
-               <h1>This is image add </h1>
+               <h1>Using Image Component</h1>
                <Image
                     src="https://testrigor.com/wp-content/uploads/2023/04/nextjs-logo.png"
                     alt="next logo"
                     width={800}
                     height={800}
                />
+
+               <h2>use Local image</h2>
+               <Image
+                    src={nextImage}
+                    alt="next logo"
+                    width={800}
+                    height={800}
+               />
+
+               
           </div>
      );
 };
